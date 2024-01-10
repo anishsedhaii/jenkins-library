f42cb57a870dc65a3bd3008cc49a40ea4071cc35
aniecesedhai <awitice23@gmail.com>
2024-01-10 16:07:04 +0545

    initialised cert pull function

---
+++ vaultcerts.groovy
@@ -0,0 +1,6 @@
+def call(String VAULT_TOKEN, String VAULT_SERVER, String VAULT_PATH){
+   def workingDir = "${WORKSPACE}"
+   sh "curl --header "X-Vault-Token: $VAULT_TOKEN" --request GET https://${VAULT_SERVER}/v1/${VAULT_PATH} | jq -r ".data.cert" > ssl/STAR_khalti_com_np_bundle.pem"
+   sh "curl --header "X-Vault-Token: $VAULT_TOKEN" --request GET https://${VAULT_SERVER}/v1/${VAULT_PATH} | jq -r ".data.cert" > ssl/STAR_khalti_com_np_bundle.pem"
+   return workingDir
+}
